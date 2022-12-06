78433af13f0b5825c59e04c49f5dc35f99140e8f
Ezra Berendsen <ezra@webbers.com>
2022-12-06 10:02:01 +0100

    Remove unnecessary if statement.

--- 2022/day6/a.ts
+++ 2022/day6/a.ts
@@ -2,22 +2,21 @@ import * as fs from "fs"
 import * as path from "path"
 
 const input = fs
-    .readFileSync(path.join(__dirname, "input.txt"))
-    .toString()
+  .readFileSync(path.join(__dirname, "input.txt"))
+  .toString()
 
 const DISTINCT_CHARACTERS = 4
 
 for (let i = 0; i < input.length; ++i) {
-    const seen: string[] = []
-    for (let j = i; j < i + DISTINCT_CHARACTERS; j++) {
-        if (input[j] !== undefined) {
-            if (!seen.find((s) => s === input[j])) {
-                seen.push(input[j])
-            }
-        }
+  const seen: string[] = []
+  for (let j = i; j < i + DISTINCT_CHARACTERS; j++) {
+    if (!seen.find((s) => s === input[j])) {
+      seen.push(input[j])
     }
-    if (seen.length === 4) {
-        console.log("Answer is:\n" + (i + DISTINCT_CHARACTERS))
-        break
-    }
+  }
+
+  if (seen.length === 4) {
+    console.log("Answer is:\n" + (i + DISTINCT_CHARACTERS))
+    break
+  }
 }
