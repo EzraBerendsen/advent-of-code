e6834732446ec1b90648e18d0cf90226dfedb8a3
E.D.N. Berendsen <ezra.berendsen@hva.nl>
2022-12-06 07:17:42 +0100

    Add day 6.

---
+++ 2022/day6/a.ts
@@ -0,0 +1,23 @@
+import * as fs from "fs"
+import * as path from "path"
+
+const input = fs
+    .readFileSync(path.join(__dirname, "input.txt"))
+    .toString()
+
+const DISTINCT_CHARACTERS = 4
+
+for (let i = 0; i < input.length; ++i) {
+    const seen: string[] = []
+    for (let j = i; j < i + DISTINCT_CHARACTERS; j++) {
+        if (input[j] !== undefined) {
+            if (!seen.find((s) => s === input[j])) {
+                seen.push(input[j])
+            }
+        }
+    }
+    if (seen.length === 4) {
+        console.log("Answer is:\n" + (i + DISTINCT_CHARACTERS))
+        break
+    }
+}
