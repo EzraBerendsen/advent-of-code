57b37c1183abe92dc10caa62f7f17ae4b223e30d
Ezra Berendsen <ednberendsen@gmail.com>
2023-12-04 14:18:31 +0100

    Finally finish day 3 part 1.

--- 2023/day3/part1.ts
+++ 2023/day3/part1.ts
@@ -1,11 +1,17 @@
 import fs from "fs";
 
-const lines = fs.readFileSync("test.txt")
+const lines = fs.readFileSync("input.txt")
     .toString()
     .trim()
     .split('\n')
 
-const numbers = new Set<number>()
+const numbers: number[] = []
+
+const positions = [
+    [-1, -1], [0, -1], [1, -1],
+    [-1, 0], [1, 0],
+    [-1, 1], [0, 1], [1, 1]
+]
 
 function isNumber(x: string) {
     return !isNaN(parseInt(x))
@@ -17,33 +23,35 @@ function isSymbol(x: string) {
 
 lines.forEach((line, y) => {
     for (let x = 0; x < line.length; x++) {
-        if (isNumber(line[x])) {
-            let number = "";
-            let j = x;
-            while (j < line.length && isNumber(line[j])) {
-                number += line[j++];
-            }
+        const char = line[x]
+        let number = ''
 
-            if (j !== x) {
-                let numberAdded = false;
-                // Check adjacent cells for symbols
-                for (let dx = -1; dx <= 1 && !numberAdded; dx++) {
-                    for (let dy = -1; dy <= 1 && !numberAdded; dy++) {
-                        if (dx === 0 && dy === 0) continue;
-                        let nx = x + dx;
-                        let ny = y + dy;
-                        if (ny >= 0 && ny < lines.length && nx >= 0 && nx < lines[ny].length) {
-                            if (isSymbol(lines[ny][nx])) {
-                                numbers.add(parseInt(number));
-                                numberAdded = true;
-                            }
-                        }
-                    }
+        if (isNumber(char)) {
+            let start = x
+            let i = x
+            while (isNumber(line[i])) {
+                number += line[i]
+                i += 1
+            }
+            let end = i - 1
+            x = end
+
+            while ((start - 1) < end) {
+                if (positions.some((pos) => {
+                    const newY = y + pos[1];
+                    const newX = start + pos[0];
+                    if (newY < 0 || newY >= lines.length) return false;
+                    if (newX < 0 || newX >= lines[newY].length) return false;
+
+                    return isSymbol(lines[newY][newX]);
+                })) {
+                    numbers.push(parseInt(number));
+                    break;
                 }
-                x = j - 1; // Skip past the current number
+                start += 1;
             }
         }
     }
 });
 
-console.log(Array.from(numbers).reduce((acc, curr) => acc + curr, 0))
+console.log(numbers.reduce((acc, curr) => acc + curr, 0))
