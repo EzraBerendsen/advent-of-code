b268f24688053a53c5cbd59e06499aa27060a687
Ezra Berendsen <ednberendsen@gmail.com>
2023-12-04 12:26:03 +0100

    Finish part 2

--- 2023/day3/part1.ts
+++ 2023/day3/part1.ts
@@ -1,33 +1,49 @@
 import fs from "fs";
 
-const possibleGames: number[] = []
-
-fs.readFileSync("input.txt")
+const lines = fs.readFileSync("test.txt")
     .toString()
     .trim()
     .split('\n')
-    .forEach((line, index) => {
-        const allSets = line.split(': ')[1]
-        const sets = allSets.split('; ')
 
-        const map = {
-            red: 0,
-            green: 0,
-            blue: 0,
+const numbers = new Set<number>()
+
+function isNumber(x: string) {
+    return !isNaN(parseInt(x))
+}
+
+function isSymbol(x: string) {
+    return x !== '.' && !isNumber(x);
+}
+
+lines.forEach((line, y) => {
+    for (let x = 0; x < line.length; x++) {
+        if (isNumber(line[x])) {
+            let number = "";
+            let j = x;
+            while (j < line.length && isNumber(line[j])) {
+                number += line[j++];
+            }
+
+            if (j !== x) {
+                let numberAdded = false;
+                // Check adjacent cells for symbols
+                for (let dx = -1; dx <= 1 && !numberAdded; dx++) {
+                    for (let dy = -1; dy <= 1 && !numberAdded; dy++) {
+                        if (dx === 0 && dy === 0) continue;
+                        let nx = x + dx;
+                        let ny = y + dy;
+                        if (ny >= 0 && ny < lines.length && nx >= 0 && nx < lines[ny].length) {
+                            if (isSymbol(lines[ny][nx])) {
+                                numbers.add(parseInt(number));
+                                numberAdded = true;
+                            }
+                        }
+                    }
+                }
+                x = j - 1; // Skip past the current number
+            }
         }
+    }
+});
 
-        sets.forEach((set) => {
-            const cubes = set.split(', ')
-            cubes.forEach((cube) => {
-                const [value, key] = cube.split(' ')
-                map[key] = Math.max(map[key], parseInt(value))
-            })
-        })
-
-        const power = Object.values(map).reduce((acc, curr) => {
-            return acc * curr
-        }, 1)
-        possibleGames.push(power)
-    })
-
-console.log(possibleGames.reduce((acc, curr) => acc + curr, 0))
+console.log(Array.from(numbers).reduce((acc, curr) => acc + curr, 0))
--- 2023/day4/part2.ts
+++ 2023/day4/part2.ts
@@ -1,9 +1,45 @@
 import fs from "fs";
 
-fs.readFileSync("test.txt")
+const scratchcards: number[] = []
+let totalScratchcards = 0
+
+function processCardLine(line: string, index: number) {
+    const currentCard = index + 1
+    const cardNumbers = line.split(': ')[1]
+    const [winningNumbers, myNumbers] = cardNumbers.split(' | ').map((numLine) =>
+        numLine.trim().split(' ')
+            .map((num) => num.trim())
+            .filter(n => n !== '')
+            .map((num) => parseInt(num)))
+
+    let count = 0
+    myNumbers.forEach((num) => {
+        if (winningNumbers.includes(num)) {
+            count += 1
+        }
+    })
+
+    totalScratchcards += 1
+
+    while (count > 0) {
+        scratchcards.push(currentCard + count)
+        count -= 1
+    }
+}
+
+const lines = fs.readFileSync("input.txt")
     .toString()
     .trim()
     .split('\n')
-    .forEach((line, index) => {
-    })
 
+lines.forEach((line, index) => {
+    processCardLine(line, index)
+
+    while (scratchcards.length > 0) {
+        const index = scratchcards.pop()!
+        processCardLine(lines[index - 1], index - 1)
+    }
+})
+
+
+console.log(`Answer: ${totalScratchcards}`)
